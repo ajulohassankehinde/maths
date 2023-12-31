55269ce299f0b9dfd2c0a4ebb1f734dd0ce2b5aa
Hassan Ajulo <jc961108@ad.jcu.edu.au>
2024-01-01 08:19:10 +1000

    first commit

---
+++ R/sum.R
@@ -0,0 +1,15 @@
+#' sum
+#'
+#' @param x The first integer.
+#' @param y The second integer.
+#'
+#' @return An interger.
+#' @export
+#'
+#' @examples sum(1,3)
+sum <- function(x,y){
+  library(GWmodel)
+  result <- x + y
+
+  return(result)
+}
